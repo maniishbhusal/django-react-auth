a4a06500ce080450fc1285e7d5d4a3e7a6c2ff9c
Manish Bhusal <manishbhusal6@gmail.com>
2024-06-20 10:17:28 +0545

    implement password change of authenticated user

--- frontend/src/pages/auth/ChangePassword.jsx
+++ frontend/src/pages/auth/ChangePassword.jsx
@@ -1,93 +1,105 @@
-import React, { useState } from 'react';
-import { Grid, Box, TextField, Button, Alert } from '@mui/material';
+import React, { useRef, useState } from "react";
+import { Grid, Box, TextField, Typography, Button, Alert } from "@mui/material";
+import changePassword from "../../services/userAuthApi";
 
 const ChangePassword = () => {
-  const [error, setError] = useState({
-    status: false,
-    msg: "",
-    type: "",
-  });
+  // State to manage server errors
+  const [serverError, setServerError] = useState({});
+  
+  // Reference to the form element
+  const formRef = useRef(null);
 
-  const handleSubmit = (e) => {
+  // Handle form submission
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const data = new FormData(e.currentTarget);
+    
+    // Create an object with the form data
     const actualData = {
-      newPassword: data.get("newPassword"),
-      confirmNewPassword: data.get("confirmNewPassword"),
+      password: data.get("newPassword"),
+      confirm_password: data.get("confirmNewPassword"),
     };
 
-    // Validation
-    if (actualData.newPassword && actualData.confirmNewPassword) {
-      if (actualData.newPassword === actualData.confirmNewPassword) {
-        setError({
-          status: true,
-          msg: "Password Changed Successfully",
-          type: "success",
-        });
-        document.getElementById("change-password-form").reset();
+    try {
+      // Call the changePassword service
+      const res = await changePassword(actualData);
+
+      // Check for errors in the response
+      if (res.errors) {
+        setServerError(res.errors);
+        console.log("error", serverError);
       } else {
-        setError({
-          status: true,
-          msg: "Passwords do not match",
-          type: "error",
-        });
+        console.log("Password changed successfully");
+        setServerError(res);
+        formRef.current.reset(); // Clear the form
       }
-    } else {
-      setError({
-        status: true,
-        msg: "All fields are required",
-        type: "error",
-      });
+    } catch (error) {
+      console.error("Error changing password", error);
     }
   };
 
   return (
-    <>
-      <Grid container justifyContent={"center"}>
-        <Grid item sm={6} xs={12}>
+    <Grid container justifyContent={"center"}>
+      <Grid item sm={6} xs={12}>
         <h1>Change Password</h1>
-          <Box
-            component="form"
-            noValidate
-            sx={{ mt: 2 }}
-            id="change-password-form"
-            onSubmit={handleSubmit}
-          >
-            <TextField
-              required
-              fullWidth
-              margin="normal"
-              id="newPassword"
-              name="newPassword"
-              label="New Password"
-              type="password"
-            />
-            <TextField
-              required
-              fullWidth
-              margin="normal"
-              id="confirmNewPassword"
-              name="confirmNewPassword"
-              label="Confirm New Password"
-              type="password"
-            />
-
-            <Box textAlign="center">
-              <Button
-                type="submit"
-                variant="contained"
-                sx={{ mt: 3, mb: 2, px: 5 }}
-              >
-                Change Password
-              </Button>
-            </Box>
-            {error.status && (
-              <Alert severity={error.type}>{error.msg}</Alert>
-            )}
+        <Box
+          component="form"
+          noValidate
+          sx={{ mt: 2 }}
+          id="change-password-form"
+          onSubmit={handleSubmit}
+          ref={formRef} // Attach the ref to the form
+        >
+          <TextField
+            required
+            fullWidth
+            margin="normal"
+            id="newPassword"
+            name="newPassword"
+            label="New Password"
+            type="password"
+          />
+          {serverError.password && (
+            <Typography
+              style={{ fontSize: 12, color: "red", paddingLeft: 10 }}
+            >
+              {serverError.password[0]}
+            </Typography>
+          )}
+          <TextField
+            required
+            fullWidth
+            margin="normal"
+            id="confirmNewPassword"
+            name="confirmNewPassword"
+            label="Confirm New Password"
+            type="password"
+          />
+          {serverError.confirm_password && (
+            <Typography
+              style={{ fontSize: 12, color: "red", paddingLeft: 10 }}
+            >
+              {serverError.confirm_password[0]}
+            </Typography>
+          )}
+          <Box textAlign="center">
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ mt: 3, mb: 2, px: 5 }}
+            >
+              Change Password
+            </Button>
           </Box>
-        </Grid>
+          {serverError.msg && (
+            <Alert severity={"success"}>{serverError.msg}</Alert>
+          )}
+          {serverError.non_field_errors && (
+            <Alert severity="error">{serverError.non_field_errors[0]}</Alert>
+          )}
+        </Box>
       </Grid>
-    </>
+    </Grid>
   );
 };
 
--- frontend/src/services/userAuthApi.js
+++ frontend/src/services/userAuthApi.js
@@ -1,5 +1,7 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import axiosInstance from "./axiosInstance";
+import { getToken } from "./localStorageService";
 
 // Define a service using a base URL and expected endpoints
 export const userAuthApi = createApi({
@@ -19,4 +21,32 @@ export const userAuthApi = createApi({
   }),
 });
 
+
+/**
+ * The function `changePassword` asynchronously sends a POST request to change a user's password with
+ * proper authorization handling.
+ * @param passwordData - The `passwordData` parameter in the `changePassword` function likely contains
+ * the new password information that the user wants to change to. It could include fields such as
+ * `oldPassword` and `newPassword` to verify the user's identity and update their password accordingly.
+ * @returns The `changePassword` function is returning the data received from the API response if the
+ * request is successful. If there is an error during the API call, it will return the error response
+ * data.
+ */
+const changePassword = async (passwordData) => {
+  try {
+    const { access_token } = getToken();
+    const res = await axiosInstance.post("changepassword/", passwordData, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    // console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return error.response.data;
+  }
+};
+
+export default changePassword;
 export const { useRegisterUserMutation } = userAuthApi;
